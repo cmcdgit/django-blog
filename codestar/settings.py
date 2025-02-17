98cd58a0d46815214d2091127ef5ea94a0aef29f
cmcdgit <cmcdgit@gmail.com>
2025-02-17 00:20:09 +0000

    Added Post model, updated settings

--- codestar/settings.py
+++ codestar/settings.py
@@ -32,7 +32,10 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.herokuapp.com',]
+ALLOWED_HOSTS = [
+    '.herokuapp.com',
+    '127.0.0.1',
+    ]
 
 
 # Application definition
@@ -93,6 +96,10 @@ DATABASES = {
     dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.herokuapp.com"
+]
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
